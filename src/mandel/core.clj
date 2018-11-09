b3d26c5e4f4edfd288b368e45cd70b8fe6b51865
Jaromir Radek <jradek@yandex.com>
2018-11-09 16:26:46 -0500

    initial 'working' version. There are still issues with certain board dimensions

--- src/mandel/core.clj
+++ src/mandel/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.io :refer [output-stream file]])
   (:gen-class))
 
+
 (defn ppm-header
   "Write the Header for a PPM File"
   [w h]
@@ -27,33 +28,41 @@
 (defn color-value [r g b]
   (byte-array  [(to-byte r) (to-byte g) (to-byte b)]))
 
-(defn sqr-complex
-  [[x y]]
-  [(- (* x x) (* y y))
-   (* 2 x y)])
 
 (defn mag-complex
+  "Magnitude of complex number"
   [[x y]]
   (+ (* x x) (* y y)))
 
-(defn add-complex
-  [[x0 y0] [x1 y1]]
-  [(+ x0 x1) (+ y0 y1)])
 
-(defn mandel-func
-  [[x0 y0] max-iterations]
-  (loop [iter 0
-         z (sqr-complex [x0 y0])]
-    (if (or (> iter max-iterations) (> (mag-complex z) 4))
-      iter
-      (recur
-        (inc iter)
-        (add-complex z [x0 y0])))))
+(defn add-complex
+  "Add two complex number represented as tuple"
+  [[re0 imag0] [re1 imag1]]
+  [(+ re0 re1) (+ imag0 imag1)])
+
+
+(defn mul-complex
+  "Multiply two complex number represented as tuple"
+  [[re0 imag0] [re1 imag1]]
+  [(- (* re0 re1) (* imag0 imag1)) (+ (* re0 imag1) (* re1 imag0))])
+
+
+(defn mandel
+  "Get number of iterations out of 'max-iterations' for complex number z0"
+  [z0 max-iterations]
+  (loop [i 0
+         z [0.0 0.0]]
+    (if (> i max-iterations)
+      max-iterations
+      (if (< (mag-complex z) 4.0)
+        (recur (inc i) (add-complex z0 (mul-complex z z)))
+        i))))
+
 
 (defn iter-to-color
   [x max-iterations]
   (cond
-    (> x max-iterations) (color-value 255 255 255)
+    (>= x max-iterations) (color-value 255 255 255)
     (<= x 63) (let [tmp (- 255 (* 4 x))]
                   (color-value 255 tmp tmp))
     :else (color-value 255 (- x 63) 0)))
@@ -61,7 +70,7 @@
 (defn iter-to-rgb
   [x max-iterations]
   (cond
-    (> x max-iterations) [255 255 255]
+    (>= x max-iterations) [255 255 255]
     (<= x 63) (let [tmp (- 255 (* 4 x))]
                 [255 tmp tmp])
     :else [255 (- x 63) 0]))
@@ -72,28 +81,31 @@
 
 
 (defn dimensions
+  "default dimensions"
   [steps]
   (let [m {:x-min -2.0 :y-min -1.5 :x-max 1.0 :y-max 1.5 :steps steps}]
     (merge m {:x-step (step (:x-min m) (:x-max m) steps)
               :y-step (step (:y-min m) (:y-max m) steps)})))
 
 
-(defn board
+(defn default-board
+  "get a default board of mandel set iterations"
   [steps max-iterations]
   (let [dim (dimensions steps)]
     (for [y (range (:y-max dim) (:y-min dim) (- 0 (:y-step dim)))
           x (range (:x-min dim) (:x-max dim) (:x-step dim))]
-      (mandel-func [x y] max-iterations))))
+      (mandel [x y] max-iterations))))
 
 
+;; TODO: some values for 'dim' do not work, such as they create shifted images, e.g. dim = 200
 (with-open
-  [out (output-stream (file "output/red.ppm"))]
-  (let [dim 500
-        max-iter 200]
+  [out (output-stream (file "output/mandel.ppm"))]
+  (let [dim 256
+        max-iter 255]
     (do
       (.write out (.getBytes (ppm-header dim dim)))
       (doseq [x (map #(iter-to-color %1 max-iter)
-                     (board dim max-iter))]
+                     (default-board dim max-iter))]
         (.write out x)))))
 
 (with-open
